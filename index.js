16f9e047f918dbf0af62dafd9b326d77d2831727
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-01-25 21:39:05 +0100

    working on feather the api

---
+++ index.js
@@ -0,0 +1,17 @@
+
+async function fetchWeather() {
+    try {
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation");
+
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const dataPoint = await response.json();
+        console.log(dataPoint);
+    }
+
+    catch {
+        
+    }
+}
