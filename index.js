0fd67bcbbe4b70eda772e80b075e135515be55bd
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-01-30 11:46:42 +0100

    working on the api of weather fetch

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 
-// async function fetchWeather() {
-//     try {
-//         const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation");
-
-//         if(!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-
-//         const dataPoint = await response.json();
-//         console.log(dataPoint);
-//     }
-
-//     catch {
-//         console.error("Error fetching data:", error);
-//     }
-// }
-
 const readInput = document.getElementById("search");
 const suggestionsBox = document.getElementById("suggestions");
+
+async function fetchWeather() {
+    try {
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation");
+
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const dataPoint = await response.json();
+        console.log(dataPoint);
+    }
+
+    catch (error ){
+        console.error("Error fetching data:", error);
+    }
+}
+
 
 readInput.addEventListener("keyup", displayCity);
 
